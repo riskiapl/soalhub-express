625fe33ee4070f0839910c315827ccdb2000e331
Riski Agung <riski97@gmail.com>
2026-04-04 17:28:59 +0700

    connect to postgres

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
+import "dotenv/config";
 import express, { type Request, type Response } from "express";
+import pkg from "pg";
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const app = express();
-const port: number = 3000;
+const port: number = Number(process.env.PORT) || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Halo! Ini server Express menggunakan TypeScript");
+});
+
+app.get("/db", async (req: Request, res: Response) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT NOW()");
+    res.json(result.rows[0]);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error mengakses database");
+  }
 });
 
 app.listen(port, () => {
